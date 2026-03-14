e03c5b971b4730ef9ce29e861775758311589a26
Legi <jasonlegister@gmail.com>
2026-03-14 15:31:00 -0700

    testing mapping error solution. May be due to no existing live server.

--- client/src/config.js
+++ client/src/config.js
@@ -1,8 +1,3 @@
-
-const BACKEND_URL = (process.env.NODE_ENV === 'production') ? '' : 'http://localhost:5000';
+const BACKEND_URL = (process.env.NODE_ENV === 'production') ? 'https://72.76.207.228:5000' : 'http://localhost:5000';
 console.log(`NODE_ENV: ${process.env.NODE_ENV}, BACKEND_URL: ${BACKEND_URL}`)
 export { BACKEND_URL };
-
-// const BACKEND_URL = (process.env.NODE_ENV === 'production') ? 'https://72.76.207.228:5000' : 'http://localhost:5000';
-// console.log(`NODE_ENV: ${process.env.NODE_ENV}, BACKEND_URL: ${BACKEND_URL}`)
-// export { BACKEND_URL };
