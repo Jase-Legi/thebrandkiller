67a956ff65d3f4cdbaaa3344ca4f214eeba56944
Legi <jasonlegister@gmail.com>
2026-03-14 10:58:46 -0700

    sw

--- client/src/config.js
+++ client/src/config.js
@@ -3,8 +3,6 @@ const BACKEND_URL = (process.env.NODE_ENV === 'production') ? '' : 'http://local
 console.log(`NODE_ENV: ${process.env.NODE_ENV}, BACKEND_URL: ${BACKEND_URL}`)
 export { BACKEND_URL };
 
-
-
 // const BACKEND_URL = (process.env.NODE_ENV === 'production') ? 'https://72.76.207.228:5000' : 'http://localhost:5000';
 // console.log(`NODE_ENV: ${process.env.NODE_ENV}, BACKEND_URL: ${BACKEND_URL}`)
 // export { BACKEND_URL };
